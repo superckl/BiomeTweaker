ddcb6b161d159b287e9eac19f9e529801f7a441e
Christopher Larsson <clarsson@wisc.edu>
2015-05-25 13:54:38 -0500

    Adding a way for other coremods to add script commands

--- src/main/java/me/superckl/biometweaker/core/BiomeTweakerCore.java
+++ src/main/java/me/superckl/biometweaker/core/BiomeTweakerCore.java
@@ -2,6 +2,9 @@ package me.superckl.biometweaker.core;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Map;
 
 import me.superckl.biometweaker.common.reference.ModData;
@@ -9,11 +12,13 @@ import squeek.asmhelper.me.superckl.biometweaker.ObfHelper;
 import cpw.mods.fml.common.asm.transformers.AccessTransformer;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.Name;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.SortingIndex;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 
 @SortingIndex(1001)
 @MCVersion("1.7.10")
+@Name("BiomeTweakerCore")
 @TransformerExclusions({"me.superckl.biometweaker.core", "me.superckl.biometweaker.util", "me.superckl.biometweaker.config", "squeek.asmhelper.me.superckl.biometweaker", "me.superckl.biometweaker.script"})
 public class BiomeTweakerCore extends AccessTransformer implements IFMLLoadingPlugin{
 
@@ -42,6 +47,32 @@ public class BiomeTweakerCore extends AccessTransformer implements IFMLLoadingPl
 	public void injectData(final Map<String, Object> data) {
 		BiomeTweakerCore.mcLocation = (File) data.get("mcLocation");
 		ObfHelper.setObfuscated((Boolean) data.get("runtimeDeobfuscationEnabled"));
+		ModBiomeTweakerCore.logger.info("Attempting to discover integration callbacks in coremods...");
+		final List coremods = (List) data.get("coremodList");
+		Field coreModInstance = null;
+		for(final Object obj:coremods)
+			try {
+				if(coreModInstance == null){
+					coreModInstance = obj.getClass().getDeclaredField("coreModInstance");
+					coreModInstance.setAccessible(true);
+				}
+				final IFMLLoadingPlugin plugin = (IFMLLoadingPlugin) coreModInstance.get(obj);
+				if(plugin == null)
+					continue;
+				final Class<?> clazz = plugin.getClass();
+				final Method method = clazz.getDeclaredMethod("btCallback");
+				ModBiomeTweakerCore.logger.info("Found integration callback in coremod "+obj.toString());
+				if((method.getModifiers() & java.lang.reflect.Modifier.STATIC) == java.lang.reflect.Modifier.STATIC)
+					method.invoke(null);
+				else
+					method.invoke(plugin);
+			}catch(final NoSuchMethodException nm){
+				//Swallow it, this means they have no callback method.
+			}catch (final Exception e) {
+				ModBiomeTweakerCore.logger.error("Failed to discover callbacks for "+obj.toString());
+				e.printStackTrace();
+				continue;
+			}
 	}
 
 	@Override

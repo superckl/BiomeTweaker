55a4fc7e151999d00c2175a08f359ee6bd3a8a8d
superckl <superckl@superckl.me>
2015-07-22 14:20:42 -0700

    Removing signing

--- src/main/java/me/superckl/biometweaker/common/reference/ModData.java
+++ src/main/java/me/superckl/biometweaker/common/reference/ModData.java
@@ -5,7 +5,6 @@ public class ModData {
 	public static final String MOD_ID = "BiomeTweaker";
 	public static final String MOD_NAME= "BiomeTweaker";
 	public static final String VERSION = "@VERSION@";
-	public static final String FINGERPRINT = "@FINGERPRINT@";
 	public static final String CLIENT_PROXY = "me.superckl.biometweaker.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "me.superckl.biometweaker.proxy.ServerProxy";
 	public static final String GUI_FACTORY = "me.superckl.biometweaker.client.gui.GuiFactory";

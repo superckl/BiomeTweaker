e46bde0cf771860501f967f111e1950d059c4da6
superckl <superckl@superckl.me>
2015-11-16 09:40:28 -0600

    More performance improvements

--- src/main/java/me/superckl/biometweaker/common/world/gen/layer/GenLayerReplacement.java
+++ src/main/java/me/superckl/biometweaker/common/world/gen/layer/GenLayerReplacement.java
@@ -1,6 +1,6 @@
 package me.superckl.biometweaker.common.world.gen.layer;
 
-import gnu.trove.map.hash.TIntIntHashMap;
+import gnu.trove.map.TIntIntMap;
 import me.superckl.biometweaker.common.handler.BiomeEventHandler;
 import net.minecraft.world.gen.layer.GenLayer;
 
@@ -16,7 +16,7 @@ public class GenLayerReplacement extends GenLayer{
 	@Override
 	public int[] getInts(final int areaX, final int areaY, final int areaWidth, final int areaHeight) {
 		final int[] ints = this.parent.getInts(areaX, areaY, areaWidth, areaHeight);
-		final TIntIntHashMap replacements = BiomeEventHandler.getBiomeReplacements();
+		final TIntIntMap replacements = BiomeEventHandler.getBiomeReplacements();
 		for(int i = 0; i < ints.length; i++)
 			if(replacements.containsKey(ints[i]))
 				ints[i] = replacements.get(ints[i]);

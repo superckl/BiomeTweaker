6322496a34b92ac552e8bce13c172f91728f1c2d
superckl <superckl@superckl.me>
2015-11-30 20:48:53 -0600

    Add "genTallPlants" to set command. #66

---
+++ src/main/java/me/superckl/biometweaker/common/world/gen/feature/WorldGenDoublePlantBlank.java
@@ -0,0 +1,16 @@
+package me.superckl.biometweaker.common.world.gen.feature;
+
+import java.util.Random;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenDoublePlant;
+
+public class WorldGenDoublePlantBlank extends WorldGenDoublePlant{
+
+	public boolean generate(World p_76484_1_, Random p_76484_2_, int p_76484_3_, int p_76484_4_, int p_76484_5_)
+    {
+        return true;
+    }
+	
+}

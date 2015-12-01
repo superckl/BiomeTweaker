d8b8d55e16097e6b2069c5c73bf40ee3ef2238b4
superckl <superckl@superckl.me>
2015-11-30 21:00:38 -0600

    Add override annot.

--- src/main/java/me/superckl/biometweaker/common/world/gen/feature/WorldGenDoublePlantBlank.java
+++ src/main/java/me/superckl/biometweaker/common/world/gen/feature/WorldGenDoublePlantBlank.java
@@ -8,6 +8,7 @@ import net.minecraft.world.gen.feature.WorldGenDoublePlant;
 
 public class WorldGenDoublePlantBlank extends WorldGenDoublePlant{
 
+	@Override
 	public boolean generate(World p_76484_1_, Random p_76484_2_, int p_76484_3_, int p_76484_4_, int p_76484_5_)
     {
         return true;

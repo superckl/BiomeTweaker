be4087d1815b480ce467e4ef9492d0bcf2333416
superckl <superckl@superckl.me>
2016-04-05 16:48:56 -0500

    addActualFillerBlock is now metadata sensitive #75

--- src/main/java/me/superckl/biometweaker/BiomeHooks.java
+++ src/main/java/me/superckl/biometweaker/BiomeHooks.java
@@ -1,13 +1,13 @@
 package me.superckl.biometweaker;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 
 public class BiomeHooks {
 
-	public static boolean contains(final Block[] blocks, final Block block){
+	public static boolean contains(final IBlockState[] blocks, final IBlockState block){
 		//LogHelper.info("Called for "+block+"in "+Arrays.toString(blocks));
-		for(final Block search:blocks)
-			if(search == block)
+		for(final IBlockState search:blocks)
+			if(search.getBlock() == block.getBlock() && search.getBlock().getMetaFromState(search) == block.getBlock().getMetaFromState(block))
 				return true;
 		return false;
 	}

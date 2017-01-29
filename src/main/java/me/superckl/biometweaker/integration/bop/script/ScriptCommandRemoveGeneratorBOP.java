2e3ce180c04bfcd3ca710c06a69b48efab338fa8
srs-bsns <forfrdm@gmail.com>
2017-01-29 00:50:03 -0500

    Readd BoP module

---
+++ src/main/java/me/superckl/biometweaker/integration/bop/script/ScriptCommandRemoveGeneratorBOP.java
@@ -0,0 +1,28 @@
+package me.superckl.biometweaker.integration.bop.script;
+
+import java.util.Iterator;
+
+import biomesoplenty.api.biome.IExtendedBiome;
+import lombok.RequiredArgsConstructor;
+import me.superckl.api.biometweaker.script.pack.IBiomePackage;
+import me.superckl.api.superscript.command.IScriptCommand;
+import me.superckl.biometweaker.integration.bop.BOPIntegrationModule;
+import net.minecraft.world.biome.Biome;
+
+@RequiredArgsConstructor
+public class ScriptCommandRemoveGeneratorBOP implements IScriptCommand{
+
+	private final IBiomePackage pack;
+	private final String type;
+
+	@Override
+	public void perform() throws Exception {
+		final Iterator<Biome> it = this.pack.getIterator();
+		while(it.hasNext()){
+			final Biome biome = it.next();
+			final IExtendedBiome eBiome = BOPIntegrationModule.getExtendedBiome(biome);
+			eBiome.getGenerationManager().removeGenerator(this.type);
+		}
+	}
+
+}

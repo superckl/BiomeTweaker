3fc045b403d044d9c2a0466f55105d1b7aef0d2b
superckl <superckl@superckl.me>
2017-07-08 16:09:14 -0500

    Suppress warnings we can't do anything about

--- src/main/java/me/superckl/biometweaker/integration/bop/BOPBiomeProperties.java
+++ src/main/java/me/superckl/biometweaker/integration/bop/BOPBiomeProperties.java
@@ -8,6 +8,7 @@ import me.superckl.api.biometweaker.property.PropertyField;
 public class BOPBiomeProperties {
 
 	public static final PropertyField<Integer> TOTAL_BIOMES_WEIGHT = new PropertyField<>(BOPClimates.class, "totalBiomesWeight", Integer.class);
+	@SuppressWarnings("rawtypes")
 	public static final PropertyField<ArrayList> LAND_BIOMES = new PropertyField<>(BOPClimates.class, "landBiomes", ArrayList.class);
 
 }

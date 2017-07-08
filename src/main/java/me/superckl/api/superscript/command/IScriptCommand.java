72523936a784470e219edaf4443e7a3e143160f6
superckl <superckl@superckl.me>
2017-07-08 16:13:03 -0500

    collapse block and block meta options into one using the new block framework. Finishes warning cleanup

--- src/main/java/me/superckl/api/superscript/command/IScriptCommand.java
+++ src/main/java/me/superckl/api/superscript/command/IScriptCommand.java
@@ -1,7 +1,10 @@
 package me.superckl.api.superscript.command;
 
+import me.superckl.api.superscript.script.ScriptHandler;
+
 public interface IScriptCommand {
 
 	public void perform() throws Exception;
+	default void setScriptHandler(final ScriptHandler handler) {}
 
 }

35d7ae23d2d3e502879672347ec2cd68e0219a66
superckl <superckl@superckl.me>
2017-08-28 09:26:30 -0700

    Restore missing functionality in blockstate objects. Fixes #123

--- src/main/java/me/superckl/biometweaker/script/object/block/BasicBlockStateScriptObject.java
+++ src/main/java/me/superckl/biometweaker/script/object/block/BasicBlockStateScriptObject.java
@@ -1,11 +1,14 @@
 package me.superckl.biometweaker.script.object.block;
 
+import java.util.Arrays;
+
 import me.superckl.api.biometweaker.block.BasicBlockStateBuilder;
 import me.superckl.api.biometweaker.script.object.BlockStateScriptObject;
 import me.superckl.api.superscript.script.ScriptHandler;
 import me.superckl.api.superscript.script.command.ScriptCommand;
 import me.superckl.api.superscript.util.CollectionHelper;
 import me.superckl.biometweaker.BiomeTweaker;
+import net.minecraft.util.ResourceLocation;
 
 public class BasicBlockStateScriptObject extends BlockStateScriptObject<BasicBlockStateBuilder>{
 
@@ -22,6 +25,13 @@ public class BasicBlockStateScriptObject extends BlockStateScriptObject<BasicBlo
 		return newArgs;
 	}
 
+	@Override
+	public void readArgs(Object... args) throws Exception {
+		if(args.length != 1 || !(args[0] instanceof String))
+			throw new IllegalArgumentException("Invalid parameters to create a block object! Objects: "+Arrays.toString(args));
+		this.builder.setrLoc(new ResourceLocation((String) args[0]));
+	}
+	
 	@Override
 	public void addCommand(final ScriptCommand command) {
 		BiomeTweaker.getInstance().addCommand(command);

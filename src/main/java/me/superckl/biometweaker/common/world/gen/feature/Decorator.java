95d539b61ead879e559cb1a24a92995e3ccccf13
superckl <superckl@superckl.me>
2017-08-29 14:35:01 -0700

    Catch any errors from decoration #127

--- src/main/java/me/superckl/biometweaker/common/world/gen/feature/Decorator.java
+++ src/main/java/me/superckl/biometweaker/common/world/gen/feature/Decorator.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 import me.superckl.api.biometweaker.world.gen.feature.WorldGeneratorWrapper;
 import me.superckl.biometweaker.common.world.gen.PlacementStage;
+import me.superckl.biometweaker.util.LogHelper;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
@@ -13,16 +14,21 @@ import net.minecraft.world.biome.Biome;
 public class Decorator {
 
 	public static void runDecoration(final PlacementStage stage, final World world, final Random rand, final ChunkPos pos){
-		final DecorationManager manager = DecorationManager.getManagerForWorld(world.provider.getDimension());
-		if(!manager.hasReplacements(stage))
-			return;
-		final BlockPos bPos = new BlockPos((pos.x << 4), 0, (pos.z << 4));
-		final int id = Biome.getIdForBiome(world.getBiome(bPos.add(8, 0, 8)));
-		if(!manager.hasReplacements(id, stage))
-			return;
-		final List<WorldGeneratorWrapper<?>> gens = manager.findDecorationList(id, stage);
-		for(final WorldGeneratorWrapper<?> gen:gens)
-			gen.generate(world, rand, bPos);
+		try {
+			final DecorationManager manager = DecorationManager.getManagerForWorld(world.provider.getDimension());
+			if(!manager.hasReplacements(stage))
+				return;
+			final BlockPos bPos = new BlockPos((pos.x << 4), 0, (pos.z << 4));
+			final int id = Biome.getIdForBiome(world.getBiome(bPos.add(8, 0, 8)));
+			if(!manager.hasReplacements(id, stage))
+				return;
+			final List<WorldGeneratorWrapper<?>> gens = manager.findDecorationList(id, stage);
+			for(final WorldGeneratorWrapper<?> gen:gens)
+				gen.generate(world, rand, bPos);
+		} catch (final Exception e) {
+			LogHelper.error("Failed to process decorate event for stage: "+stage);
+			e.printStackTrace();
+		}
 	}
 
 }

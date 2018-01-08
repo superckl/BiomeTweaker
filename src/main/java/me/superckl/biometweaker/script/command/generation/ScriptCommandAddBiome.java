0e5cc41531199ec9141c2e251b0a534cad3d980d
superckl <superckl@superckl.me>
2018-01-07 23:08:12 -0800

    Allow all vanilla biomes to be copied. Closes #159

--- src/main/java/me/superckl/biometweaker/script/command/generation/ScriptCommandAddBiome.java
+++ src/main/java/me/superckl/biometweaker/script/command/generation/ScriptCommandAddBiome.java
@@ -1,13 +1,19 @@
 package me.superckl.biometweaker.script.command.generation;
 
 import java.lang.reflect.Constructor;
+import java.util.IdentityHashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.Lists;
+import com.google.common.primitives.Primitives;
 
 import lombok.RequiredArgsConstructor;
 import me.superckl.api.biometweaker.event.BiomeTweakEvent;
 import me.superckl.api.biometweaker.property.BiomePropertyManager;
 import me.superckl.api.biometweaker.property.Property;
+import me.superckl.api.biometweaker.property.PropertyField;
 import me.superckl.api.biometweaker.script.AutoRegister;
 import me.superckl.api.biometweaker.script.pack.BiomePackage;
 import me.superckl.api.superscript.script.command.ScriptCommand;
@@ -17,16 +23,37 @@ import me.superckl.biometweaker.common.world.biome.BiomeTweakerBiome;
 import me.superckl.biometweaker.script.object.TweakerScriptObject;
 import me.superckl.biometweaker.util.BiomeHelper;
 import me.superckl.biometweaker.util.LogHelper;
+import me.superckl.biometweaker.util.ObfNameHelper;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biome.BiomeProperties;
 import net.minecraft.world.biome.Biome.SpawnListEntry;
+import net.minecraft.world.biome.BiomeForest;
+import net.minecraft.world.biome.BiomeHills;
+import net.minecraft.world.biome.BiomeJungle;
+import net.minecraft.world.biome.BiomeMesa;
+import net.minecraft.world.biome.BiomePlains;
+import net.minecraft.world.biome.BiomeSnow;
+import net.minecraft.world.biome.BiomeTaiga;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.MinecraftForge;
 
 @AutoRegister(classes = TweakerScriptObject.class, name = "createBiome")
 @RequiredArgsConstructor
 public class ScriptCommandAddBiome extends ScriptCommand{
+
+	public static final Map<Class<? extends Biome>, List<? extends Property<?>>> extraParameters = new IdentityHashMap<>();
+
+	static {
+		ScriptCommandAddBiome.extraParameters.put(BiomeForest.class, Lists.newArrayList(new PropertyField<>(BiomeForest.class, ObfNameHelper.Fields.BIOMEFOREST_TYPE.getName(), BiomeForest.Type.class)));
+		ScriptCommandAddBiome.extraParameters.put(BiomeTaiga.class, Lists.newArrayList(new PropertyField<>(BiomeTaiga.class, ObfNameHelper.Fields.BIOMETAIGA_TYPE.getName(), BiomeTaiga.Type.class)));
+		ScriptCommandAddBiome.extraParameters.put(BiomeHills.class, Lists.newArrayList(new PropertyField<>(BiomeHills.class, ObfNameHelper.Fields.BIOMEHILLS_TYPE.getName(), BiomeHills.Type.class)));
+		ScriptCommandAddBiome.extraParameters.put(BiomeSnow.class, Lists.newArrayList(new PropertyField<>(BiomeSnow.class, ObfNameHelper.Fields.SUPERICY.getName(), Boolean.class)));
+		ScriptCommandAddBiome.extraParameters.put(BiomePlains.class, Lists.newArrayList(new PropertyField<>(BiomePlains.class, ObfNameHelper.Fields.SUNFLOWERS.getName(), Boolean.class)));
+		ScriptCommandAddBiome.extraParameters.put(BiomeMesa.class, Lists.newArrayList(new PropertyField<>(BiomeMesa.class, ObfNameHelper.Fields.BRYCEPILLARS.getName(), Boolean.class),
+				new PropertyField<>(BiomeMesa.class, ObfNameHelper.Fields.HASFOREST.getName(), Boolean.class)));
+		ScriptCommandAddBiome.extraParameters.put(BiomeJungle.class, Lists.newArrayList(new PropertyField<>(BiomeJungle.class, ObfNameHelper.Fields.ISEDGE.getName(), Boolean.class)));
+	}
 
 	private final String rLoc;
 	private final BiomePackage toCopy;
@@ -53,8 +80,16 @@ public class ScriptCommandAddBiome extends ScriptCommand{
 				LogHelper.warn("More than one biome found to copy! Only the first one will be copied.");
 			Constructor<? extends Biome> construct = null;
 			try{
-				//catches most vanilla biomes, a few exceptions
-				construct = toCopy.getBiomeClass().getConstructor(BiomeProperties.class);
+				//catches all vanilla biomes
+				if(ScriptCommandAddBiome.extraParameters.containsKey(toCopy.getBiomeClass())) {
+					final List<? extends Property<?>> props = ScriptCommandAddBiome.extraParameters.get(toCopy.getBiomeClass());
+					final Class<?>[] types = new Class<?>[props.size()+1];
+					for(int i = 0; i < props.size(); i++)
+						types[i] = Primitives.unwrap(props.get(i).getTypeClass());
+					types[types.length-1] = BiomeProperties.class;
+					construct = toCopy.getBiomeClass().getConstructor(types);
+				}else
+					construct = toCopy.getBiomeClass().getConstructor(BiomeProperties.class);
 			} catch(final Exception e){
 				try{
 					//Catches most BOP biomes
@@ -65,10 +100,23 @@ public class ScriptCommandAddBiome extends ScriptCommand{
 			if(construct == null){
 				LogHelper.warn("Unable to copy biome class "+toCopy.getBiomeClass().getCanonicalName()+"! Some functionality may not be copied!");
 				biome = new BiomeTweakerBiome(new BiomeProperties("BiomeTweaker Biome").setBaseHeight(0.125F).setHeightVariation(0.05F).setTemperature(0.8F).setRainfall(0.4F));
-			}else{
-				final boolean noArgs = construct.getParameterCount() == 0;
-				biome = noArgs ? construct.newInstance():construct.newInstance(new BiomeProperties(toCopy.getBiomeName()));
-			}
+			} else
+				switch(construct.getParameterCount()) {
+				case 0:
+					biome = construct.newInstance();
+					break;
+				case 1:
+					biome = construct.newInstance(new BiomeProperties(toCopy.getBiomeName()));
+					break;
+				default:
+					final List<? extends Property<?>> props = ScriptCommandAddBiome.extraParameters.get(toCopy.getBiomeClass());
+					final Object[] objs = new Object[props.size()+1];
+					for(int i = 0; i < props.size(); i++)
+						objs[i] = props.get(i).get(toCopy);
+					objs[objs.length-1] = new BiomeProperties(toCopy.getBiomeName());
+					biome = construct.newInstance(objs);
+					break;
+				}
 			if(MinecraftForge.EVENT_BUS.post(new BiomeTweakEvent.Create(this, biome)))
 				return;
 			Biome.registerBiome(id, ModData.MOD_ID+":"+this.rLoc.toLowerCase(), biome);
